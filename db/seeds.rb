b16b25e295482818a57e3f7d30d5102c060e3a8b
Krista Loven <krista.loven.87@gmail.com>
2020-01-15 15:03:18 -0800

    Seed projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.create([
+  {
+    name: 'Crazy Eights',
+    languages: 'Ruby | Active Record | SQLite | Deck Of Cards API | Colorize',
+    description: '​CLI implementation of the classic card game',
+    visual: '',
+    frontend_repo: '',
+    backend_repo: 'https://github.com/kel29/guided-module-one-project-assessment-seattle-web-082619?fbclid=IwAR2LyT6flk_tnSazL7WAn99Vc6WzkiWUv58IfN2CWB364XQ8MOoOSMDGj0s',
+    url: ''
+  },
+  {
+    name: 'Chat',
+    languages: 'JavaScript | React | Bootstrap | Ruby on Rails | ActionCable',
+    description: '​Socket-based group chat application',
+    visual: '',
+    frontend_repo: 'https://github.com/kel29/chat-frontend',
+    backend_repo: 'https://github.com/kel29/chat-server',
+    url: ''
+  },
+  {
+    name: 'Telephone',
+    languages: 'JavaScript | React Native | Expo | Native Base | Ruby on Rails | S3',
+    description: 'Entertaining party game application which applies drawing to the childhood classic',
+    visual: '',
+    frontend_repo: 'https://github.com/kel29/frontend-telephone',
+    backend_repo: 'https://github.com/kel29/Telephone-server',
+    url: ''
+  }
+])
